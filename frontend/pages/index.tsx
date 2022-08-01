c66d54c174c7ab3a6a83192d5845ea20d2766c3b
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-08-01 22:44:55 +0430

    Tailwind setup completed

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Home = () => {
-  return <>
-    <h1>Whitelist Dapp</h1>
-  </>
+  return <main className="bg-black h-screen" >
+    <h1 className="text-white text-3xl">Whitelist Dapp</h1>
+  </main>
 }
 
 export default Home
