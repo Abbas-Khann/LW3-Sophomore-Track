20be7acefdef3ee3c53ca931abcd87c8a3416cad
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-08-04 19:51:08 +0430

    Token deployed

--- Backend/scripts/KhaNFTDeploy.ts
+++ Backend/scripts/KhaNFTDeploy.ts
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+// const { ethers } = require("hardhat");
 require("dotenv").config({ path: "../.env" });
 const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../Constants/constants");
 
---
+++ Backend/scripts/TokenDeploy.ts
@@ -0,0 +1,23 @@
+const { ethers } = require("hardhat")
+require("dotenv").config({ path: "../.env" });
+const { NFT_CONTRACT_ADDRESS } = require("../Constants/constants");
+
+const Mainn = async (): Promise <any> => {
+    const KhaNFTContract =  NFT_CONTRACT_ADDRESS;
+
+    const KHANFT_TOKEN = await ethers.getContractFactory("KhaNFTtoken");
+
+    const deployedKhaNFTtoken = await KHANFT_TOKEN.deploy(
+        KhaNFTContract
+    );
+
+    console.log("KhaNFT TOKEN CONTRACT ADDRESS :",
+                 deployedKhaNFTtoken.address
+               );
+
+}
+
+Mainn().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
