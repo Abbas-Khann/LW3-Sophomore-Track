ef1082bc9f42c27d6bf57525955bf7bf53512093
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-08-04 18:44:43 +0430

    Completed KhaNFT

--- frontend/pages/KhaNFT.tsx
+++ frontend/pages/KhaNFT.tsx
@@ -1,7 +1,7 @@
 import {useState, useEffect, useRef} from 'react';
 import Head from '../node_modules/next/head';
 import Web3Modal from 'web3modal';
-import { providers, Contract } from '../node_modules/ethers/lib/ethers';
+import { providers, Contract, utils } from '../node_modules/ethers/lib/ethers';
 import { KhaNFTContractAddress, KHANFTCONTRACTABI } from '../Constants/constants';
 import { toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
